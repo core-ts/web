6b88ea10ee9a57635e976e0954b7db4be48cbb11
Duc Nguyen <minhduc140583@gmail.com>
2026-04-12 22:41:56 +0700

    Add toMenuItems

--- src/index.ts
+++ src/index.ts
@@ -330,9 +330,6 @@ export function formatNumber(v?: number | null, scale?: number, d?: string | nul
   }
 }
 
-export function getDateFormat(profile?: string): string {
-  return "M/D/YYYY"
-}
 export function getPage(page?: string): number {
   const num = getNumber(page)
   return num === undefined || num < 1 ? 1 : num
@@ -553,4 +550,84 @@ export class formatter {
 }
 export function formatPhone(phone?: string | null): string {
   return formatter.formatPhone(phone)
+}
+
+export interface MenuItem {
+  id: string
+  name: string
+  path: string
+  resource?: string
+  icon?: string
+  sequence?: number
+  prefetch?: boolean
+  type?: string
+  children?: MenuItem[]
+}
+export interface Category {
+  id: string
+  name: string
+  path: string
+  resource?: string
+  icon?: string
+  sequence?: number
+  prefetch?: boolean
+  type?: string
+  parent?: string
+  children?: MenuItem[]
+}
+export function rebuildPath(items: MenuItem[], lang: string) {
+  for (const item of items) {
+    item.path = item.type === "content" ? `/${lang}${item.path}` : `${item.path}?lang=${lang}`
+    const children = item.children
+    if (children && children.length > 0) {
+      rebuildPath(children, lang)
+    }
+  }
+}
+
+export function sub(n1?: number, n2?: number): number {
+  if (!n1 && !n2) {
+    return 0
+  } else if (n1 && n2) {
+    return n1 - n2
+  } else if (n1) {
+    return n1
+  } else if (n2) {
+    return -n2
+  }
+  return 0
+}
+function subMenuItem(p1: MenuItem, p2: MenuItem): number {
+  return sub(p1.sequence, p2.sequence)
+}
+export function toMenuItems(m: Category[]): MenuItem[] {
+  const ps: Category[] = getRoot(m)
+  for (const p of ps) {
+    getChildren(p, m)
+  }
+  return ps.sort(subMenuItem)
+}
+function getRoot(ms: Category[]): Category[] {
+  const ps: Category[] = []
+  for (const m of ms) {
+    if (!m.parent || m.parent.length === 0) {
+      delete m.parent
+      ps.push(m)
+    }
+  }
+  return ps.sort(subMenuItem)
+}
+function getChildren(m: Category, all: Category[]) {
+  const children: MenuItem[] = []
+  for (const s of all) {
+    if (s.parent === m.id) {
+      delete s.parent
+      children.push(s)
+      getChildren(s, all)
+    }
+  }
+  if (children.length > 0) {
+    children.sort(subMenuItem)
+    m.children = children
+  }
 }
